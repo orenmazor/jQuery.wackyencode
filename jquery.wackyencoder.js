bcf7cb0574bfff3bd481fa856dd2bf0f84fa2cb2
Oren Mazor <oren.mazor@gmail.com>
2011-07-14 13:25:38 -0400

    initial working version, because Brad said I couldn't

---
+++ jquery.wackyencoder.js
@@ -0,0 +1,22 @@
+(function($){
+    $.fn.wackyencode = function() {
+        
+        var text = this[0].innerText;
+        //No lower case support here
+        text = text.toUpperCase();
+        
+        //browsers are wacky. they reverse the base calculation, so we reverse ahead of time.
+        //it'd be too easy if strings supported the reverse function
+        text = text.split('').reverse().join('');
+        
+        var result = 0;
+        
+        for(i=0;i<text.length;i++)
+        {
+            var characterValue = text.charCodeAt(i) - 64;
+            result += characterValue * Math.pow(26,i);
+        }
+        var replacement = "<ol style=\"list-style-type: upper-alpha;\" start=\""+result+"\"><li></li></ol>"
+        $(this.selector).html(replacement);
+    };
+})(jQuery);
